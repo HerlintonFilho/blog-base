eb3ae8090f33dc0b1e661be2c9875c81e1311701
HerlintonFilho <herlintoborba5@gmail.com>
2022-06-23 13:30:13 -0300

    iniciando a paginação

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -65,5 +65,51 @@ router.get("/admin/articles/edit/:id", (req, res) =>{
     })
 })
 
+router.post("/articles/update", (req, res) =>{
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category
+    Article.update({title: title, body: body, categoryId: category, slug: slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    });
+})
+
+router.get("/articles/page/:num",(req, res)=> {
+    var page = req.params.num;
+    var offset = 0
+    if(isNaN(page) || page == 1 ){
+        offset = 0;
+    }else{
+        offset = parseInt(page) * 4;
+    }
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset
+    }).then(articles =>{
+        var next;
+        if(offset + 4 >= articles.count){
+            next = false
+        }else{
+            next = true
+        }
+        var result = {
+            next : next,
+            articles : articles
+        }
+
+        Category.findAll().then(categories =>{
+            res.render("admin/articles/page", {result: result, categories: categories})
+        })
+        
+    })
+})
+
 
 module.exports = router;
